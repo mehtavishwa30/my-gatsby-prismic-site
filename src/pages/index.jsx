870903273425051f5538f24d463d97cb30dd7621
Vishwa Mehta <32997409+mehtavishwa30@users.noreply.github.com>
2019-07-31 18:06:01 +0530

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,19 +4,69 @@ import { Parser } from 'html-to-react'
 import React from 'react'
 import Layout from '../components/Layout'
 
+const container = css`
+  margin: 0 auto;
+  padding: 4rem 2rem 8rem 2rem;
+  max-width: 600px;
+  color: #333333;
+
+  h1 {
+    font-size: 30px;
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-bottom: 2rem;
+  }
+`
+
 const htmlToReactParser = new Parser()
 
 export default props => {
   const { data } = props
   const content = data.prismicHomepage.data
-  const name = content.name.text
-  const description = content.description.html
+  const page_title = content.page_title.text
+  const page_subtitle = content.page_subtitle.html
+
+  const slices = content.body.map(slice => {
+    const headline = slice.primary.headline.html
+    const picture = slice.primary.picture.url
+    const description = slice.primary.description.html
+    const items = slice.items.map(item => {
+      htmlToReactParser.parse(item.description.html)
+      htmlToReactParser.parse(item.number.html)
+    })
+
+    if (slice.slice_type === 'Image') {
+      return (
+        <div className="Image">
+          <div>{picture}</div>
+        </div>
+      )
+    }
+    if (slice.slice_type === 'Info') {
+      return (
+        <div className="Info">
+          <h1>{htmlToReactParser.parse(headline)}</h1>
+          <h2>{picture}</h2>
+          <div>{htmlToReactParser.parse(description)}</div>
+        </div>
+      )
+    }
+    if (slice.slice_type === 'Facts') {
+      return (
+        <div className="Facts">
+          <h1>{htmlToReactParser.parse(headline)}</h1>
+          <h2>{htmlToReactParser.parse(description)}</h2>
+          <div>{items}</div>
+        </div>
+      )
+    }
+  })
 
   return (
     <Layout>
-      <div>
-        <h1>{name}</h1>
-        {htmlToReactParser.parse(description)}
+      <div css={container}>
+        <h1>{page_title}</h1>
+        {htmlToReactParser.parse(page_subtitle)}
       </div>
     </Layout>
   )
@@ -26,11 +76,58 @@ export const pageQuery = graphql`
   query {
     prismicHomepage {
       data {
-        name {
+        page_title {
           text
         }
-        description {
+        page_subtitle {
           html
+        }
+        body {
+          ... on PrismicHomepageBodyImage {
+            slice_type
+            primary {
+              picture {
+                url
+              }
+            }
+          }
+        }
+        body {
+          ... on PrismicHomepageBodyInfo {
+            slice_type
+            primary {
+              headline {
+                html
+              }
+              picture {
+                url
+              }
+              description {
+                html
+              }
+            }
+          }
+        }
+        body {
+          ... on PrismicHomepageBodyFacts {
+            slice_type
+            primary {
+              headline {
+                html
+              }
+              description {
+                html
+              }
+            }
+            items {
+              number {
+                html
+              }
+              description {
+                html
+              }
+            }
+          }
         }
       }
     }
